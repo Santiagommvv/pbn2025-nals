dc0977e4e0dd3a9f1a19aac2d88ebe2f9a31c37c
Alejandro <alejandrojaviercardozo2002@hotmail.com>
2025-05-25 23:37:29 -0300

    Implementacion del menu interactivo, funcion agregra y cerrar menu, correciones de nodo

--- main.c
+++ main.c
@@ -2,8 +2,58 @@
 #include "include/listaES.h" //definir bandera si queremos sacar include/ de la cadena
 #include "include/nodo.h"
 
+extern Lista miLista;
+
 int main(){
 
-    printf("hola, por el momento soy un place-holder del punto de entrada de la aplicación");
+    int opcion;
+    do{
+    printf(
+        "\nElije que desea realizar:\n"
+        "1) agregar un numero a la lista\n"
+        "2) eliminar un elemento de la lista\n"
+        "3) buscar un elemento de la lista\n"
+        "4) imprimir los elementos de la lista\n"
+        "5) salir del programa\n\n"); 
+        scanf("%d", &opcion);
 
+    if (opcion < 1 || opcion > 5){
+        printf("\nopcion incorrecta, elija un numero del 1 al 5\n");
+        continue;
+    }
+
+
+    switch (opcion){
+    case 1:
+        int valor;
+        printf("\nIngrese el numero que desea agregar\n");
+        scanf("%d", &valor);
+        insertarAlInicio(&miLista.cabeza,valor);
+        break;
+    case 2:
+        printf("eliminar un elemento de la lista");
+    case 3:
+        printf("buscar un elemento de la lista"); 
+    case 4:     
+        printf("imprimir los elementos de la lista");  
+    
+    case 5:{
+        int salir;
+        printf(
+            "\n¿esta seguro que desea salir de la lista?\n" 
+            "1) SI\n"
+            "2) NO\n");
+        scanf("%d", &salir);
+        if (salir==1){
+           printf("Adios :)\n");
+        return 0;
+         }else if(salir == 2){
+           printf("\nVolviendo al menu principal\n");
+            continue;
+        }else{
+           printf("\nLa opcion es incorrecta, volviendo al menu principal\n");
+        }
+      }
+    }
+  }while(1);
 }
--- src/nodo.c
+++ src/nodo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/nodo.h"
+#include "nodo.h"
 
 // Crea un nuevo nodo
 Nodo* crearNodo(int dato) {
@@ -14,39 +14,4 @@ Nodo* crearNodo(int dato) {
     nuevoNodo->siguienteNodo = NULL;
 
     return nuevoNodo;
-}
-
-//La funcion te permite saber que tiene almacenado el nodo actual.Devuelve su contenido.
-int* getDato(Nodo* actual){
-    if (actual != NULL && actual->dato != NULL){
-        return actual->dato;
-    }
-    return NULL;
-}
-
-//La funcion te permite elegir a quien apunta el nodo actual
-void setNext(Nodo* actual, Nodo* siguiente){
-    if(actual != NULL){
-        actual->siguienteNodo = siguiente;
-    }
-}
-
-//La funcion devuelve el proximo nodo si es que lo hay
-Nodo* next(Nodo* actual) {
-    if (hasNext(actual)){
-        return actual->siguienteNodo;
-    }
-    return NULL;
-}
-
-//La funcion te permite saber si hay un nodo siguiente 
-int hasNext(Nodo* actual){
-    if (actual != NULL && actual->siguienteNodo != NULL){
-        return 1;
-    }
-    return 0;
-}
-
-void eliminarNodo(Nodo* actual){
-
 }
