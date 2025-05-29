a1f5035cd6b652dc1b82391570bc70a5ca5375aa
Palacios Segovia Lucas <lugapase@gmail.com>
2025-05-28 22:54:35 -0300

    Agregue las opciones faltantes del main

--- main.c
+++ main.c
@@ -1,56 +1,75 @@
 #include <stdio.h>
-#include "include/listaES.h" //definir bandera si queremos sacar include/ de la cadena
+#include "include/listaES.h"
 #include "include/nodo.h"
 
 extern Lista miLista;
 
-int main(){
-
+int main() {
     int opcion;
-    do{
-    printf(
-        "\nElije que desea realizar:\n"
-        "1) agregar un numero a la lista\n"
-        "2) eliminar un elemento de la lista\n"
-        "3) buscar un elemento de la lista\n"
-        "4) imprimir los elementos de la lista\n"
-        "5) salir del programa\n\n"); 
+    int valor;
+
+    do {
+        printf(
+            "\nElije que desea realizar:\n"
+            "1) agregar un numero a la lista\n"
+            "2) eliminar un elemento de la lista\n"
+            "3) buscar un elemento de la lista\n"
+            "4) imprimir los elementos de la lista\n"
+            "5) salir del programa\n\n");
         scanf("%d", &opcion);
 
-    switch (opcion){
-    case 1:
-        int valor;
-        printf("\nIngrese el numero que desea agregar\n");
-        scanf("%d", &valor);
-        insertarAlInicio(&miLista.cabeza,valor);
-        break;
-    case 2:
-        printf("eliminar un elemento de la lista");
-    case 3:
-        printf("buscar un elemento de la lista"); 
-    case 4:     
-        printf("imprimir los elementos de la lista");  
-    
-    case 5:
-        int salir;
-        printf(
-            "\n¿Esta seguro que desea salir de la lista?\n" 
-            "1) SI\n"
-            "2) NO\n");
-        scanf("%d", &salir);
-        if (salir == 1){
-           printf("Adios :)\n");
-        return 0;
-         }else if (salir == 2){
-           printf("\nVolviendo al menu principal\n");
-            continue;
-        }else{
-           printf("\nLa opcion es incorrecta, volviendo al menu principal\n");
+        switch (opcion) {
+        case 1: {
+            printf("\nIngrese el numero que desea agregar\n");
+            scanf("%d", &valor);
+            insertarAlInicio(&miLista.cabeza, valor);
+            break;
         }
-      
-    default:  
-        printf("\nopcion incorrecta, elija un numero del 1 al 5\n");
-        break;
-    }
-  }while(1);
+        case 2: {
+            printf("Eliminar un elemento de la lista\n");
+            scanf("%d", &valor);
+            eliminarNodo(&miLista.cabeza, valor);
+            break;
+        }
+        case 3: {
+            printf("Buscar un elemento de la lista\n");
+            scanf("%d", &valor);
+            Nodo* encontrado = buscarNodo(miLista.cabeza, valor);
+            if (encontrado) {
+                printf("Elemento %d encontrado en la lista.\n", valor);
+            } else {
+                printf("Elemento %d no encontrado en la lista.\n", valor);
+            }
+            break;
+        }
+        case 4: {
+            printf("Elementos de la lista:\n");
+            imprimirLista(miLista.cabeza);
+            break;
+        }
+        case 5: {
+            int salir;
+            printf(
+                "\n¿Está seguro que desea salir del programa?\n"
+                "1) SI\n"
+                "2) NO\n");
+            scanf("%d", &salir);
+            if (salir == 1) {
+                printf("Adios :)\n");
+                return 0;
+            } else if (salir == 2) {
+                printf("\nVolviendo al menu principal\n");
+                continue;
+            } else {
+                printf("\nOpción incorrecta, volviendo al menu principal\n");
+            }
+            break;
+        }
+        default:
+            printf("\nOpción incorrecta, elija un número del 1 al 5\n");
+            break;
+        }
+    } while (1);
+
+    return 0;
 }
